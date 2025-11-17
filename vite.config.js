3a6b5f57882dd702d72834f3032db6c89af3bfa4
anders0825 <gah082025@gmail.com>
2025-11-17 10:17:41 +0100

    init

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/211125_react_op2_states/",
+});
